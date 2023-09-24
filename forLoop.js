f06b3e90fdbd560596cfbfe80a89e9582061ea5a
unknown <ashikbur007@gmail.com>
2023-09-24 20:23:49 +0600

    loop

---
+++ forLoop.js
@@ -0,0 +1,42 @@
+const webTechs = [
+    'HTML',
+    'CSS12',
+    'JavaScript',
+    'React11',
+    'Redux',
+    'Node13',
+    'MongoDB'
+  ]
+
+  let newArray =[];
+//   let j = 0;
+for (let i = 0; i < webTechs.length; i++) {
+
+     newArray = webTechs[i].length;
+     if(newArray.length <= newArray.length){
+        console.log(newArray);
+     }
+    
+}
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+// let newArray = webTechs.map(tech =>[tech,tech.length]);
+// let mernStack = webTechs.map(tech =>tech[0]).join('');
+// let mernStack1 = webTechs.sort() ;
+// let mernStack2 = webTechs.reverse() ;
+    
+     
+
+//   console.log(newArray);
+//   console.log(mernStack2);
